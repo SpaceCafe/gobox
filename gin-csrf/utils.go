a641bd274b3ac20173c7866fae59cc026bbc8868
Lars Thoms <lars@thoms.io>
2024-07-29 08:11:01 +0200

    🎨 gin-csrf: name return values in HeaderToken function

--- gin-csrf/utils.go
+++ gin-csrf/utils.go
@@ -7,7 +7,7 @@ import (
 // HeaderToken retrieves the CSRF token and its associated header name from the context.
 // It returns the header name and the encoded token if they are present in the context,
 // otherwise it returns empty strings.
-func HeaderToken(ctx *gin.Context) (string, string) {
+func HeaderToken(ctx *gin.Context) (headerName, encodedToken string) {
 	config, okConfig := ctx.Get("csrf/config")
 	token, okToken := ctx.Get("csrf/token")
 
