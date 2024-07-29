c5627e05e68f7961d3333975abfe212b7a29a69b
Lars Thoms <lars@thoms.io>
2024-07-29 10:10:37 +0200

    🐛 logger: add ParseLevel alias to logger functions

--- logger/logger_default.go
+++ logger/logger_default.go
@@ -10,20 +10,21 @@ func Default() *Logger {
 
 // Aliases of logger functions.
 var (
-	SetFormat = std.SetFormat
-	SetLevel  = std.SetLevel
-	SetOutput = std.SetOutput
-	Output    = std.Output
-	Debug     = std.Debug
-	Debugf    = std.Debugf
-	Info      = std.Info
-	Infof     = std.Infof
-	Warning   = std.Warning
-	Warningf  = std.Warningf
-	Warn      = std.Warn
-	Warnf     = std.Warnf
-	Error     = std.Error
-	Errorf    = std.Errorf
-	Fatal     = std.Fatal
-	Fatalf    = std.Fatalf
+	SetFormat  = std.SetFormat
+	SetLevel   = std.SetLevel
+	ParseLevel = std.ParseLevel
+	SetOutput  = std.SetOutput
+	Output     = std.Output
+	Debug      = std.Debug
+	Debugf     = std.Debugf
+	Info       = std.Info
+	Infof      = std.Infof
+	Warning    = std.Warning
+	Warningf   = std.Warningf
+	Warn       = std.Warn
+	Warnf      = std.Warnf
+	Error      = std.Error
+	Errorf     = std.Errorf
+	Fatal      = std.Fatal
+	Fatalf     = std.Fatalf
 )
