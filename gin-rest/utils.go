267ea19d60cf15b950466e278ad88e96fe9a205e
Lars Thoms <lars@thoms.io>
2024-09-13 14:00:11 +0200

    🐛 gin-rest: fix bug by adding len=0

--- gin-rest/utils.go
+++ gin-rest/utils.go
@@ -7,7 +7,7 @@ import (
 // joinMapKeys takes a map with string keys and empty struct values, and a separator string.
 // It returns a single string with all the keys joined by the separator.
 func joinMapKeys(m map[string]any, sep string) string {
-	keys := make([]string, len(m))
+	keys := make([]string, 0, len(m))
 	for key := range m {
 		keys = append(keys, key)
 	}
