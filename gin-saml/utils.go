bdd79fe7685ee847fc76bdf0592a5e49785dcc5e
Lars Thoms <lars@thoms.io>
2024-09-24 12:39:45 +0200

    🧑‍💻 saml: add GetAll method to retrieve all mapped attributes

--- gin-saml/utils.go
+++ gin-saml/utils.go
@@ -11,11 +11,14 @@ var (
 	ErrNoSessionAttributes = errors.New("no session attributes found")
 )
 
+// Attributes is a type that embeds samlsp.Attributes and includes a configuration pointer.
 type Attributes struct {
 	samlsp.Attributes
 	config *Config
 }
 
+// GetAttributes retrieves SAML attributes from the session context.
+// It returns an error if no session attributes are found.
 func (r *SAML) GetAttributes(ctx *gin.Context) (attributes *Attributes, err error) {
 	session, _ := ctx.Get("saml.session")
 	sessionAttributes, ok := session.(samlsp.SessionWithAttributes)
@@ -27,11 +30,23 @@ func (r *SAML) GetAttributes(ctx *gin.Context) (attributes *Attributes, err erro
 	return
 }
 
-// Get returns the first mapped attribute named `name` or an empty string if
-// no such attributes is present.
+// Get returns the first mapped attribute named `name` or
+// an empty string if no such attributes is present.
 func (r *Attributes) Get(name string) string {
 	if key, ok := r.config.Mapping[name]; ok {
 		return r.Attributes.Get(key)
 	}
 	return r.Attributes.Get(name)
+}
+
+// GetAll returns all mapped attributes named `name` or
+// an empty []string if no such attributes is present.
+func (r *Attributes) GetAll(name string) []string {
+	if key, ok := r.config.Mapping[name]; ok {
+		name = key
+	}
+	if r.Attributes == nil || len(r.Attributes[name]) == 0 {
+		return []string{}
+	}
+	return r.Attributes[name]
 }
