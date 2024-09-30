d63a320d9925b5f796e89f9855ab0f0ddbe331a7
Lars Thoms <lars@thoms.io>
2024-09-30 12:57:27 +0200

    ♻️ gin-authorization: add `GetAuthorizations`

--- gin-authorization/utils.go
+++ gin-authorization/utils.go
@@ -5,14 +5,22 @@ import (
 	problems "github.com/spacecafe/gobox/gin-problems"
 )
 
-// IsAuthorized checks if a given action on a resource is authorized for the current context.
-func IsAuthorized(ctx *gin.Context, resource string, action Action) bool {
+// GetAuthorizations retrieves the Authorizations object from the context.
+// It returns an empty Authorizations object if not found.
+func GetAuthorizations(ctx *gin.Context) Authorizations {
 	if authorizations, ok := ctx.Get("authorization/authorizations"); ok {
-		return authorizations.(Authorizations).IsAuthorized(resource, action)
+		return authorizations.(Authorizations)
 	}
-	return false
+	return Authorizations{}
 }
 
+// IsAuthorized checks if a given action on a resource is authorized for the current context.
+func IsAuthorized(ctx *gin.Context, resource string, action Action) bool {
+	return GetAuthorizations(ctx).IsAuthorized(resource, action)
+}
+
+// RequireAuthorization creates a middleware handler that ensures the request is authorized
+// for the specified resource and action. If not authorized, it aborts the request with an error.
 func RequireAuthorization(resource string, action Action) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		if IsAuthorized(ctx, resource, action) {
